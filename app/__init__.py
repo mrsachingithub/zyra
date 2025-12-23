c104dc4c7a2ad4d212744b03e5f539ea10bfdb65
Sachin Kumar <sachinsagar7632@gmail.com>
2025-12-23 13:44:26 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from config import Config
+from .config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
